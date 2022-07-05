3f1945cca9e3b8356a8e54546b2a73a5baea335e
cleber cavalcanti <cleberscavalcanti@hotmail.com>
2022-07-05 16:20:53 -0300

    Confguração de rede provisória

---
+++ subnets.tf
@@ -0,0 +1,6 @@
+resource "aws_subnet" "Net2_Priv" {
+  vpc_id                  = aws_vpc.DevDev.id
+  cidr_block              = "172.29.8.0/21"
+  availability_zone       = us-east-1
+  map_public_ip_on_launch = false
+}
