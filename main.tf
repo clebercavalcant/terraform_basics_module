8c18c2249fc2e9910c17b9ed56f934f5a615f093
cleber cavalcanti <cleberscavalcanti@hotmail.com>
2022-07-05 16:10:20 -0300

    Alteração region

---
+++ main.tf
@@ -0,0 +1,14 @@
+#provider "aws" {
+#   profile = "default"
+#    region = var.region
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
